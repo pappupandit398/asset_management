a5d04636dd4dd1d74cfc954a2f2ff8b62fd8aac3
Dev Arya <devarya398@gmail.com>
2021-03-07 17:54:55 +0530

    my first commit

---
+++ target/classes/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO employee(full_name, designation) values ('Dev Arya', 'Software Developer'),
+('Ravi Kumar', 'QA Engineer'), ('Rakesh Kumar', 'Software Developer');
+
+INSERT INTO category(name, description) values ('Electronics', 'This category is for all electronics items'),
+('Furniture', 'This category is for all furniture items');
